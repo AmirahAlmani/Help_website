d191fb6727a11667511f1f804928cecf3606e0f5
Amirah Almani <amirahalmani@gmail.com>
2020-04-04 23:05:42 +0300

    created the user model seeded the database

--- help_website/db/seeds.rb
+++ help_website/db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(firstName:"Mohamme" , lastName:"Ahmed" , email:"moh@gmail.com" , phone:"0505050505" , numberOfFamilyMembers:5 , region:"Riyadh")
+User.create(firstName:"sara" , lastName:"Ahmed" , email:"sara@gmail.com" , phone:"0505059999" , numberOfFamilyMembers:8 , region:"Jeddah")
